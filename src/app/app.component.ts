e1e839c82f213fecf08ed9555a2af29f92a13562
Waldo Sobrino <waldo2810@gmail.com>
2023-04-03 23:01:41 -0500

    set up for remote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServerService } from './service/server.service';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { DataState } from './enum/data-state.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit { 
+  appState$: Observable<AppState<CustomResponse>>;
+  constructor(private serverService: ServerService) {}
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$.pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED_STATE, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) =>
+        of({ dataState: DataState.ERROR_STATE, error })
+      )
+    );
+  }
 }
---
+++ src/app/interface/custom-response.ts
@@ -0,0 +1,14 @@
+import { Server } from './server';
+
+export interface CustomResponse {
+  timestamp: Date;
+  statusCode: number;
+  status: string;
+  reason: string;
+  message: string;
+  developerMessage: string;
+  data: {
+    servers?: [];
+    server?: Server;
+  };
+}
